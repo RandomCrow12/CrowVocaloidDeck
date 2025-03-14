cb7bd76ce8ebd3a83eefbe594d6933cc55ae6d38
RandomCrow12 <123201070+RandomCrow12@users.noreply.github.com>
2025-03-13 22:56:44 -0500

    v1.1

--- VocaloidDeck/VocaloidDeck.lua
+++ VocaloidDeck/VocaloidDeck.lua
@@ -1,59 +1,19 @@
 --- STEAMODDED HEADER
---- MOD_NAME: Vocaloid Deck
---- MOD_ID: vocaloiddeck
---- PREFIX: vocal
---- MOD_AUTHOR: [nolo33lp]
---- MOD_DESCRIPTION: Replaces default face cards king, queen, jack, to miku, teto, rin, len, neru, and luka, respectively.
+--- MOD_NAME: Guilty Gear Face Cards
+--- MOD_ID: GuiltyGearFaceCards
+--- MOD_AUTHOR: [LambdasFriend, nolo33lp, RandomCrow]
+--- MOD_DESCRIPTION: Changes face cards to GG characters
 
-------------------------------------------------
-------------------------------------------------
+----------------------------------------------
+------------MOD CODE -------------------------
 
-ranks = {"Jack", "Queen", "King"}
-SMODS.Atlas {
-	key = 'vocaloidplace',
-	px = 71,
-	py = 95,
-	disable_mipmap = true,
-	path = 'vocaloiddeck.png'
-    }
-	
+function SMODS.INIT.DecColors()
 
-SMODS.DeckSkin ({
-	key = 'Kasane Teto',
-	suit = "Hearts",
-	ranks = ranks,
-	lc_atlas = 'vocal_vocaloidplace',
-	hc_atlas = 'vocal_vocaloidplace',
-	pos_style = 'deck'
-	})
+    local dec_mod = SMODS.findModByID("GuiltyGearFaceCards")
+    local sprite_card = SMODS.Sprite:new("cards_1", dec_mod.path, "VocaloidDeck.png", 71, 95, "asset_atli")
+    
+    sprite_card:register()
+end
 
-SMODS.DeckSkin ({
-	key = 'Hatsune Miku',
-	suit = "Clubs",
-	ranks = ranks,
-	lc_atlas = 'vocal_vocaloidplace',
-	hc_atlas = 'vocal_vocaloidplace',
-	pos_style = 'deck'
-	})
-
-SMODS.DeckSkin ({
-	key = 'Gumi',
-	suit = "Spades",
-	ranks = ranks,
-	lc_atlas = 'vocal_vocaloidplace',
-	hc_atlas = 'vocal_vocaloidplace',
-	pos_style = 'deck'
-	})
-
-SMODS.DeckSkin ({
-	key = 'Rin Len Neru',
-	suit = "Diamonds",
-	ranks = ranks,
-	lc_atlas = 'vocal_vocaloidplace',
-	hc_atlas = 'vocal_vocaloidplace',
-	pos_style = 'deck'
-	})
-
-
-------------------------------------------------
-------------------------------------------------
+----------------------------------------------
+------------MOD CODE END----------------------
